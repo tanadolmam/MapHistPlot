6b3a3beb7bedecd111b1830fb92c31fe030fea7b
tanadolmam <41603890+tanadolmam@users.noreply.github.com>
2018-07-25 16:26:21 +0700

    Add files via upload

--- lib/mapTool.py
+++ lib/mapTool.py
@@ -23,32 +23,27 @@ def mercLat2Norm(lat):
     new= math.log(x) * 180.0/math.pi
     return new
 
-def bgColor(zoom,opacity=130):
-	print("bgColor: ",zoom)
-	zoom,xmin,xmax,ymin,ymax=getTileBound(zoom)
+def bgColor(zoomRange,opacity=130):
+	print("bgColor: ",zoomRange)
+	zoom,xmin,xmax,ymin,ymax=getTileBound(zoomRange)
 	totalFile = []
 	for x in range(xmin,xmax+1):
 		for y in range(ymin,ymax+1):
 			imgName = "output/zoom{0}/temp{0}/{0}-{1}.png".format(zoom,str(x)+str(y))
-			# imgName = "output/zoom10/temp10/test.png" 
 			saveFile="output/zoom{0}/{0}-{1}.png".format(zoom,str(x)+str(y)) 
-			# print(imgName)
 			if (os.path.isfile(imgName)):
 				# break
 				foreground = Image.open(imgName)
-				# background = Image.open(random.choice(bgcolor))
-				# background = background.resize((foreground.size), Image.ANTIALIAS)
 				background = Image.new('RGBA', foreground.size,(0,0,0,opacity))
 				background.paste(foreground, (0, 0), foreground)
 				temp=background
 				
-				# print('saving image... ',saveFile)  
 				temp.save(saveFile,"PNG")
-			else:
-				print('image not found '+imgName)
-def retouch(zoom,brightness=1,contrast=1,color=1,sharpness=1):
-	print('retouch: ',zoom)
-	zoom,xmin,xmax,ymin,ymax=getTileBound(zoom)
+			# else:
+			# 	print('image not found '+imgName)
+def retouch(zoomRange,brightness=1,contrast=1,color=1,sharpness=1):
+	print('retouch: ',zoomRange)
+	zoom,xmin,xmax,ymin,ymax=getTileBound(zoomRange)
 	for x in range(xmin,xmax+1):
 		for y in range(ymin,ymax+1):
 		
@@ -58,12 +53,11 @@ def retouch(zoom,brightness=1,contrast=1,color=1,sharpness=1):
 			if (os.path.isfile(imgName)==True):
 				# break
 				k = Image.open(imgName)
-				# foreground.show()
-				
+
 				k= ImageEnhance.Contrast(k).enhance(contrast)
 				k= ImageEnhance.Color(k).enhance(color)
 				k= ImageEnhance.Sharpness(k).enhance(sharpness)
 				k= ImageEnhance.Brightness(k).enhance(brightness)
 				k.save(saveFile)
-			else:
-				print('file not found')
+			# else:
+			# 	print('file not found')
