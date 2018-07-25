f4bcc03551a6cfbeee5e8b1afdb50c049b8b53e9
tanadolmam <41603890+tanadolmam@users.noreply.github.com>
2018-07-25 16:26:45 +0700

    Add files via upload

--- lib/pathCheck.py
+++ lib/pathCheck.py
@@ -5,20 +5,6 @@ def createFolder(filePath):
 		os.makedirs(filePath)
 		print('create folder: ',filePath)
 
-# for i in range(1,21):
-# 	zoomFolder = "zoom{}".format(i)
-# 	dataFolder = "zoom{0}/data{0}".format(i)
-# 	tempFolder = "zoom{0}/temp{0}".format(i)
 
-# 	if not os.path.exists(zoomFolder):
-# 		os.makedirs(zoomFolder)
 
-# 	if not os.path.exists(dataFolder):
-# 		os.makedirs(dataFolder)
-
-# 	if not os.path.exists(tempFolder):
-# 		os.makedirs(tempFolder)
-
-# if not os.path.exists("TMSimage"):
-# 	os.makedirs("TMSimage")
 
